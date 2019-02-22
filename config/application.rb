03b34cb581c02a0fb172209e6d5fa9869eac090e
Franco Segura <fsegu002@gmail.com>
2019-02-21 22:05:46 -0500

    changed some configs

--- config/application.rb
+++ config/application.rb
@@ -34,11 +34,5 @@ module FutnewsApi
     config.api_only = true
 
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
   end
 end
